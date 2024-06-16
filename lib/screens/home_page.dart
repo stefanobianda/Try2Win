b3852f986e5f8ef3c247e3ab635cdc281c86235e
Stefano Bianda <stefanobianda@ik.me>
2024-06-16 10:53:30 +0200

    simple scaffold with AppBar

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatelessWidget {
+  const HomePageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Try 2 Win"),
+      ),
+      body: const Text("Try 2 Win"),
+    );
+  }
+}
