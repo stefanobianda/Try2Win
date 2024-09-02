c8d60746dbc596fc674e93a5701960aca474eb51
Stefano Bianda <stefanobianda@ik.me>
2024-09-02 11:45:17 +0200

    enable test on github action

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,45 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:try2win/business/coupon_bo.dart';
 
-import 'package:try2win/main.dart';
+import 'package:try2win/models/campaign.dart';
+import 'package:try2win/models/coupon.dart';
+import 'package:try2win/models/seller.dart';
+import 'package:try2win/widgets/coupons_list.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('CouponList widget test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Try2WinApp());
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: CouponsList(
+          couponsList: [
+            CouponBO(
+                campaign: Campaign(
+                    campaignId: "campaignId",
+                    createdAt: Timestamp.now(),
+                    name: "Pippo Pluto"),
+                coupon: Coupon(
+                    sellerId: "sellerId",
+                    customerId: "customerId",
+                    campaignId: "campaignId",
+                    value: 50,
+                    issuedAt: Timestamp.now(),
+                    used: false),
+                supplier: Seller(
+                  title: 'SellerX',
+                  ticketLimit: 10,
+                ))
+          ],
+        ),
+      ),
+    ));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Pippo Pluto'), findsOneWidget);
   });
 }
