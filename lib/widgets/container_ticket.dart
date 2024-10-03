5d2de698b07af85f475a0335a46a21a38b93abfc
Stefano Bianda <stefanobianda@ik.me>
2024-10-03 10:58:04 +0200

    simple adjustment and update of system

--- lib/widgets/container_ticket.dart
+++ lib/widgets/container_ticket.dart
@@ -11,10 +11,12 @@ class ContainerTicket extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        borderRadius: const BorderRadius.all(Radius.circular(30)),
         color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
       ),
       margin: const EdgeInsets.all(5),
+      height: 100,
+      width: 400,
       child: Column(
         children: [
           Text(
